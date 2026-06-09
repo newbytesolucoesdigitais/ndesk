88cd707e9904bc9232cfa9dc0d6527418eb259ec
puppimcaua-debug <puppimcaua@gmail.com>
2026-06-09 19:13:41 +0100

    feat(csat): expose satisfaction_ratable on the ticket REST payload

--- app/models/ticket/assets.rb
+++ app/models/ticket/assets.rb
@@ -57,4 +57,16 @@ returns
 
     false
   end
+
+  def filter_unauthorized_attributes(attributes)
+    attributes = super
+
+    user_id = UserInfo.current_user_id
+    if user_id.present? && Setting.get('csat_integration')
+      user = User.lookup(id: user_id)
+      attributes['satisfaction_ratable'] = user.present? && Ticket::SatisfactionRating.ratable?(ticket: self, user:)
+    end
+
+    attributes
+  end
 end
---
+++ spec/requests/ticket_satisfaction_ratable_spec.rb
@@ -0,0 +1,43 @@
+# spec/requests/ticket_satisfaction_ratable_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Ticket payload satisfaction_ratable', type: :request do
+  let(:group)    { create(:group) }
+  let(:agent)    { create(:agent, groups: [group]) }
+  let(:customer) { create(:customer) }
+  let(:state)    { Ticket::State.find_by(name: 'closed') }
+  let(:ticket)   { create(:ticket, group:, customer:, owner: agent, state:) }
+
+  before { Setting.set('csat_integration', true) }
+
+  def ticket_asset(response_body, ticket_id)
+    JSON.parse(response_body).dig('assets', 'Ticket', ticket_id.to_s)
+  end
+
+  it 'exposes satisfaction_ratable=true to the ticket customer when ratable' do
+    authenticated_as(customer)
+    get "/api/v1/tickets/#{ticket.id}?all=true", as: :json
+    expect(response).to have_http_status(:ok)
+    expect(ticket_asset(response.body, ticket.id)['satisfaction_ratable']).to be(true)
+  end
+
+  it 'is not true for an agent viewing the same ticket' do
+    authenticated_as(agent)
+    get "/api/v1/tickets/#{ticket.id}?all=true", as: :json
+    expect(ticket_asset(response.body, ticket.id)['satisfaction_ratable']).not_to be(true)
+  end
+
+  it 'is false for the customer when CSAT is disabled' do
+    Setting.set('csat_integration', false)
+    authenticated_as(customer)
+    get "/api/v1/tickets/#{ticket.id}?all=true", as: :json
+    expect(ticket_asset(response.body, ticket.id)['satisfaction_ratable']).to be_falsey
+  end
+
+  it 'is false for the customer once a rating exists' do
+    create(:ticket_satisfaction_rating, ticket:, customer:)
+    authenticated_as(customer)
+    get "/api/v1/tickets/#{ticket.id}?all=true", as: :json
+    expect(ticket_asset(response.body, ticket.id)['satisfaction_ratable']).to be(false)
+  end
+end
